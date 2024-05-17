3e1df634e669cd5ac10e2531e5113cb9d049eb06
William H. Grover <wgrover@gmail.com>
2024-05-17 15:55:13 -0700

    includes math to convert 2 bytes to SLPM

--- arduino/renesas_i2c/renesas_i2c.ino
+++ arduino/renesas_i2c/renesas_i2c.ino
@@ -1,3 +1,6 @@
+// FS2012-1100-NG flow sensor from Renesas
+// 0.015 to 10 Standard Liters Per Minute (SLPM)
+
 #include <Wire.h>
 
 void setup() {
@@ -9,9 +12,11 @@ void loop() {
   Wire.requestFrom(7, 2);    // request 2 bytes from peripheral device #7
 
   while (Wire.available()) { // peripheral may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
+    uint8_t b1 = Wire.read();
+    uint8_t b2 = Wire.read();
+    uint16_t val = (b1 << 8) | b2;
+    float rate = val/1000.0;  // SLPM
+    Serial.println(rate);         // print the character
   }
-
   delay(500);
 }
